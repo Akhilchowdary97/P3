c2fa835b5f77c7703cc3065e3339004068eb609b
akhil chowdary <akhilchowdary.m007@gmail.com>
2020-07-20 19:11:58 +0530

    first commit

---
+++ p3/views.py
@@ -0,0 +1,8 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+def index(request):
+    return HttpResponse("<marquee>Hello ,Welcome To p3 Project</marquee>")
+def home(request):
+    return render(request,"simple.html")
+def second(request):
+    return render(request,"directory/second.html")
